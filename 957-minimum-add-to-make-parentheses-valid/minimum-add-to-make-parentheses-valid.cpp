85c7f2063ea3be8bc96e8d01211ebdeb46d9d6d8
Saksham Joshi <sakshamjoshi2729@gmail.com>
2026-07-28 23:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.51%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(!st.empty()&& (st.top()=='(' && s[i]==')')){
+                st.pop();
+            }else st.push(s[i]);
+        }
+        int count=0;
+        while(!st.empty()){
+            count++;
+            st.pop();
+        }
+        return count;
+    }
+};
